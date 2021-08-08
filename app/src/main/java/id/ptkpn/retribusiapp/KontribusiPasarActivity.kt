0df75be3f6de427191bd5068302f0bb8477a6d13
anonGTF <anongtf26@gmail.com>
2021-08-09 06:45:22 +0700

    feat: setup dependency, firebase, and some activities

---
+++ app/src/main/java/id/ptkpn/retribusiapp/KontribusiPasarActivity.kt
@@ -0,0 +1,11 @@
+package id.ptkpn.retribusiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KontribusiPasarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kontribusi_pasar)
+    }
+}
