4687642f0ab291a65cc3ed1c66862160aae7f8d5
anonGTF <anongtf26@gmail.com>
2021-08-10 10:26:13 +0700

    feat: login logic finished

--- app/src/main/java/id/ptkpn/retribusiapp/LoginActivity.kt
+++ app/src/main/java/id/ptkpn/retribusiapp/LoginActivity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import id.ptkpn.retribusiapp.databinding.ActivityKontribusiPasarBinding
 import id.ptkpn.retribusiapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private val auth = Firebase.auth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +24,39 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
-            intent = Intent(this, MainMenuActivity::class.java)
-            startActivity(intent)
+            showLoadingState()
+            processLogin()
         }
+    }
+
+    private fun processLogin() {
+        val username = binding.etUserName.text.toString()
+        val password = binding.etPassword.text.toString()
+        val email = "$username@ptkpn.id"
+
+        if (username.isNotBlank() && password.isNotBlank()) {
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+                intent = Intent(this, MainMenuActivity::class.java)
+                startActivity(intent)
+            } .addOnFailureListener {
+                showErrorMessage(it.localizedMessage ?: "unknown error")
+            } .addOnCompleteListener {
+                hideLoadingState()
+            }
+        } else {
+            showErrorMessage("username dan password tidak boleh kosong")
+        }
+    }
+
+    private fun showErrorMessage(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    private fun showLoadingState() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideLoadingState() {
+        binding.progressBar.visibility = View.INVISIBLE
     }
 }
--- app/src/main/java/id/ptkpn/retribusiapp/SplashActivity.kt
+++ app/src/main/java/id/ptkpn/retribusiapp/SplashActivity.kt
@@ -4,13 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+     private val auth = Firebase.auth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         this.recreate()
-        startActivity(Intent(this, LoginActivity::class.java))
+        intent = if (auth.currentUser != null) {
+            Intent(this, MainActivity::class.java)
+        } else {
+            Intent(this, LoginActivity::class.java)
+        }
+        startActivity(intent)
         finish()
     }
 }
